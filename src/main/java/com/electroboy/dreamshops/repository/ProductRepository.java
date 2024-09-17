7911eee451cff8928dc0907ecf18dc96074859d3
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-09-17 22:47:28 +0500

    Implemented Product details

---
+++ src/main/java/com/electroboy/dreamshops/repository/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.electroboy.dreamshops.repository;
+
+import com.electroboy.dreamshops.model.Product;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  List<Product> findByCategoryName(String category);
+
+  List<Product> findByBrand(String brand);
+
+  List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+  List<Product> findByName(String name);
+
+  List<Product> findByBrandAndName(String brand, String name);
+
+  Long countByBrandAndName(String brand, String name);
+
+  boolean existsByNameAndBrand(String name, String brand);
+}
---
+++ src/main/java/com/electroboy/dreamshops/service/product/ProductService.java
@@ -0,0 +1,143 @@
+package com.electroboy.dreamshops.service.product;
+
+import com.electroboy.dreamshops.dto.ImageDto;
+import com.electroboy.dreamshops.dto.ProductDto;
+import com.electroboy.dreamshops.exceptions.AlreadyExistsException;
+import com.electroboy.dreamshops.exceptions.ResourceNotFoundException;
+import com.electroboy.dreamshops.model.Category;
+import com.electroboy.dreamshops.model.Image;
+import com.electroboy.dreamshops.model.Product;
+import com.electroboy.dreamshops.repository.CategoryRepository;
+import com.electroboy.dreamshops.repository.ImageRepository;
+import com.electroboy.dreamshops.repository.ProductRepository;
+import com.electroboy.dreamshops.request.AddProductRequest;
+import com.electroboy.dreamshops.request.ProductUpdateRequest;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService {
+
+  private final ProductRepository productRepository;
+  private final CategoryRepository categoryRepository;
+  private final ImageRepository imageRepository;
+  private final ModelMapper modelMapper;
+
+  @Override
+  public Product addProduct(AddProductRequest request) {
+    if (productExists(request.getName(), request.getBrand())){
+      throw new AlreadyExistsException(request.getBrand() +" "+request.getName()+ " already exists, you may update this product instead!");
+    }
+    Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+        .orElseGet(() -> {
+          Category newCategory = new Category(request.getCategory().getName());
+          return categoryRepository.save(newCategory);
+        });
+    request.setCategory(category);
+    return productRepository.save(createProduct(request, category));
+  }
+
+  private boolean productExists(String name , String brand) {
+    return productRepository.existsByNameAndBrand(name, brand);
+  }
+
+  private Product createProduct(AddProductRequest request, Category category) {
+    return new Product(
+        request.getName(),
+        request.getBrand(),
+        request.getPrice(),
+        request.getInventory(),
+        request.getDescription(),
+        category
+    );
+  }
+
+  @Override
+  public Product getProductById(Long id) {
+    return productRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+  }
+
+  @Override
+  public void deleteProductById(Long id) {
+    productRepository.findById(id)
+        .ifPresentOrElse(productRepository::delete, ()-> {
+          throw new ResourceNotFoundException("Product not found");
+        });
+  }
+
+  @Override
+  public Product updateProduct(ProductUpdateRequest request, Long productId) {
+    return productRepository.findById(productId)
+        .map(existingProduct -> updateExistingProduct(existingProduct, request))
+        .map(productRepository::save)
+        .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+  }
+
+  private Product updateExistingProduct(Product existingProduct, ProductUpdateRequest request) {
+    existingProduct.setName(request.getName());
+    existingProduct.setBrand(request.getBrand());
+    existingProduct.setPrice(request.getPrice());
+    existingProduct.setInventory(request.getInventory());
+    existingProduct.setDescription(request.getDescription());
+
+    Category category = categoryRepository.findByName(request.getCategory().getName());
+    existingProduct.setCategory(category);
+    return existingProduct;
+  }
+
+  @Override
+  public List<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  @Override
+  public List<Product> getProductsByCategory(String category) {
+    return productRepository.findByCategoryName(category);
+  }
+
+  @Override
+  public List<Product> getProductsByBrand(String brand) {
+    return productRepository.findByBrand(brand);
+  }
+
+  @Override
+  public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+    return productRepository.findByCategoryNameAndBrand(category, brand);
+  }
+
+  @Override
+  public List<Product> getProductsByName(String name) {
+    return productRepository.findByName(name);
+  }
+
+  @Override
+  public List<Product> getProductsByBrandAndName(String brand, String name) {
+    return productRepository.findByBrandAndName(brand, name);
+  }
+
+  @Override
+  public Long countProductsByBrandAndName(String brand, String name) {
+    return productRepository.countByBrandAndName(brand, name);
+  }
+
+  @Override
+  public List<ProductDto> getConvertedProducts(List<Product> products) {
+    return products.stream().map(this::convertToDto).toList();
+  }
+
+  @Override
+  public ProductDto convertToDto(Product product) {
+    ProductDto productDto = modelMapper.map(product, ProductDto.class);
+    List<Image> images = imageRepository.findByProductId(product.getId());
+    List<ImageDto> imageDtos = images.stream()
+        .map(image -> modelMapper.map(image, ImageDto.class))
+        .toList();
+    productDto.setImages(imageDtos);
+    return productDto;
+  }
+}

53ea25b852c2fa0569c17e5ea81f8f196534d1f2
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-09-17 22:49:34 +0500

    Implemented Order and OrderItem details

---
+++ src/main/java/com/electroboy/dreamshops/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.electroboy.dreamshops.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/electroboy/dreamshops/service/order/OrderService.java
@@ -0,0 +1,87 @@
+package com.electroboy.dreamshops.service.order;
+
+import com.electroboy.dreamshops.dto.OrderDto;
+import com.electroboy.dreamshops.enums.OrderStatus;
+import com.electroboy.dreamshops.exceptions.ResourceNotFoundException;
+import com.electroboy.dreamshops.model.Cart;
+import com.electroboy.dreamshops.model.Order;
+import com.electroboy.dreamshops.model.OrderItem;
+import com.electroboy.dreamshops.model.Product;
+import com.electroboy.dreamshops.repository.OrderRepository;
+import com.electroboy.dreamshops.repository.ProductRepository;
+import com.electroboy.dreamshops.service.cart.CartService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService implements IOrderService{
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final CartService cartService;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Order placeOrder(Long userId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        Order order = createOrder(cart);
+        List<OrderItem> orderItemList = createOrderItems(order, cart);
+        order.setOrderItems(new HashSet<>(orderItemList));
+        order.setTotalAmount(calculateTotalAmount(orderItemList));
+        Order savedOrder = orderRepository.save(order);
+        cartService.clearCart(cart.getId());
+        return savedOrder;
+    }
+
+    private Order createOrder(Cart cart) {
+        Order order = new Order();
+        order.setUser(cart.getUser());
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        return order;
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart) {
+        return cart.getItems().stream().map(cartItem -> {
+            Product product = cartItem.getProduct();
+            product.setInventory(product.getInventory() - cartItem.getQuantity());
+            productRepository.save(product);
+            return new OrderItem(
+                    order,
+                    product,
+                    cartItem.getQuantity(),
+                    cartItem.getUnitPrice());
+        }).toList();
+    }
+
+    private BigDecimal calculateTotalAmount(List<OrderItem> orderItemList) {
+        return orderItemList
+                .stream()
+                .map(item -> item.getPrice().multiply(new BigDecimal(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Override
+    public OrderDto getOrder(Long orderId) {
+        return orderRepository.findById(orderId)
+                .map(this::convertToDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+    }
+
+    @Override
+    public List<OrderDto> getUserOrders(Long userId) {
+        List<Order> orders = orderRepository.findByUserId(userId);
+        return  orders.stream().map(this :: convertToDto).toList();
+    }
+
+    @Override
+    public OrderDto convertToDto(Order order) {
+        return modelMapper.map(order, OrderDto.class);
+    }
+}
